cc8ec345a621b70896ef514826edd7cad9e368bc
Jari Hämäläinen <nuumiofi@gmail.com>
2024-05-23 18:43:32 +0300

    initial commit after serverpod create
    Flutter project is removed because but repro doesn't need it.

---
+++ id_only_model_sql_error_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:id_only_model_sql_error_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
